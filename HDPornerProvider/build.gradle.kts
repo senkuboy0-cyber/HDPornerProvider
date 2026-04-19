458352e5871bd9e03be96d86e046dcbf39aed5c9
senkuboy0-cyber <senkuboy0@gmail.com>
2026-04-19 12:24:58 +0600

    Initial HDPornerProvider CloudStream plugin

---
+++ HDPornerProvider/build.gradle.kts
@@ -0,0 +1,12 @@
+cloudstream {
+    setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/senkuboy0-cyber/HDPornerProvider")
+    version = 1
+    description = "HDPorner - Free Premium Porn Videos"
+    authors = listOf("senkuboy0-cyber")
+    language = "en"
+    tvTypes = listOf("Anime", "TvSeries", "Movie")
+}
+
+android {
+    namespace = "com.hdporner"
+}
